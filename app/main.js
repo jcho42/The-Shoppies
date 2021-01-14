677eeb8d10185c791324b8647eb5e2adc213fab4
jcho42 <jason.cho.301230@gmail.com>
2021-01-14 13:02:39 -0600

    feat: adds redux

--- app/main.js
+++ app/main.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 ReactDOM.render(
-  <div>{process.env.NODE_ENV}</div>,
+  <Provider store={store}>
+    <div>{process.env.NODE_ENV}</div>
+  </Provider>,
   document.getElementById('app')
 )
