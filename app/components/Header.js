80a442616fd6e331fec5d57ba430f267dec6e1a0
jcho42 <jason.cho.301230@gmail.com>
2021-01-17 17:52:19 -0600

    feat: adds icon and badge to noms list button

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { updateSearch, toggleList, clearSearch } from '../redux/reducer';
 import { IconContext } from 'react-icons';
 import { IoSearch, IoClose } from 'react-icons/io5';
 import { FaTrophy } from 'react-icons/fa';
+import { BiCameraMovie } from 'react-icons/bi'
 
-const Header = ({ searchValue, update, toggle, clear }) => {
+const Header = ({ searchValue, update, toggle, clear, noms }) => {
+  const isInitial = useRef(true);
+
+  useEffect(() => {
+    if (isInitial.current) isInitial.current = false
+  }, []);
+
   return (
     <div className="p-5 px-20 bg-black bg-opacity-90 sticky top-0 flex justify-between items-center z-20">
       <div>
@@ -38,7 +45,10 @@ const Header = ({ searchValue, update, toggle, clear }) => {
             placeholder="Search Movie Title"
             className="bg-grey-900 border-gray-100 border border-l-0 border-r-0 focus:outline-none text-sm p-1 w-52"
           />
-          <div onClick={clear} className="border border-grey-100 bg-grey-900 border-l-0 p-1 mr-10">
+          <div
+            onClick={clear}
+            className="border border-grey-100 bg-grey-900 border-l-0 p-1 mr-10"
+          >
             <IconContext.Provider value={{ color: '#F3F4F6', size: 25 }}>
               <label htmlFor="searchbar" className="cursor-pointer">
                 <IoClose />
@@ -47,10 +57,16 @@ const Header = ({ searchValue, update, toggle, clear }) => {
           </div>
         </div>
         <button
-          className="border border-gray-100 p-1 rounded focus:outline-none hover:bg-yellow-500 hover:text-black transition duration-500 ease-in-out"
+          className="border border-gray-100 p-1 rounded focus:outline-none hover:bg-yellow-500 hover:text-black transition duration-500 ease-in-out flex items-center"
           onClick={toggle}
         >
-          Nominations List
+          <span className="text-2xl">
+            <BiCameraMovie />
+          </span>
+          <span className="mx-2">Nominations List</span>
+          <span className={`bg-yellow-500 py-0.5 px-2 text-sm rounded-2xl text-black font-bold border-2 border-grey-100 ${isInitial.current && "opacity-0"}`}>
+            {noms.length}
+          </span>
         </button>
       </div>
     </div>
@@ -59,11 +75,12 @@ const Header = ({ searchValue, update, toggle, clear }) => {
 
 const mapState = (state) => ({
   searchValue: state.search,
+  noms: state.nominations,
 });
 const mapDispatch = (dispatch) => ({
   update: (value) => dispatch(updateSearch(value)),
   toggle: () => dispatch(toggleList()),
-  clear: () => dispatch(clearSearch())
+  clear: () => dispatch(clearSearch()),
 });
 
 export default connect(mapState, mapDispatch)(Header);
